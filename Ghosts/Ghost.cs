8a9725a5d2fdb7ad4c0208be72b68e0f272898ab
Roberto C Navarro <rnavarro70@gmail.com>
2018-03-05 16:00:54 -0600

    first commit

---
+++ Ghosts/Ghost.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+public class Ghost
+{
+    // ▄ ▀ █
+
+    // Fields
+    private int _x, _y;
+    private ConsoleColor _color;
+    private string[] _shape = new string[3]
+    {
+            "▄███▄",
+            "█@█@█",
+            "█▀█▀█"
+    };
+
+    // Properties
+    // ...
+
+
+    // Methods
+    public Ghost(int x, int y, ConsoleColor color)
+    {
+        // Constructor
+        _x = x;
+        _y = y;
+        _color = color;
+    }
+
+    public void Plot()
+    {
+        Console.ForegroundColor = _color;
+        for (int i = 0; i < _shape.Length; i++)
+        {
+            Console.SetCursorPosition(_x, _y + i);
+            Console.WriteLine(_shape[i]);
+        }
+        Console.ResetColor();
+    }
+}
---
+++ Ghosts/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.CursorVisible = false;
+        Console.Clear();
+
+        // Add Ghosts
+        Ghost redGhost = new Ghost(10, 10, ConsoleColor.Red);
+        redGhost.Plot();
+
+        Ghost greenGhost = new Ghost(20, 10, ConsoleColor.Green);
+        greenGhost.Plot();
+
+        Ghost yellowGhost = new Ghost(30, 10, ConsoleColor.Yellow);
+        yellowGhost.Plot();
+
+        Ghost blueGhost = new Ghost(40, 10, ConsoleColor.Blue);
+        blueGhost.Plot();
+
+
+        Console.CursorVisible = true;
+        Console.ReadLine();
+
+    }
+}
